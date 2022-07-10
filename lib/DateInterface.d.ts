87dd8b7dc80613254db968c202ea65373b2d5bf7
yzeek <isaac.cohen1978@gmail.com>
2022-07-10 23:07:31 +0300

    more commit

---
+++ lib/DateInterface.d.ts
@@ -0,0 +1,15 @@
+declare type Formats = import('./FormatesEnum').Formats;
+interface Date {
+    /**
+     *  returns a date string formatted as the requested format.
+     *  compatible with datetime-local input
+     */
+    toFormat(format: Formats): string;
+}
+declare function pad2(n: number): string;
+declare function to_year_month_date_hours_minutes(date: Date): string;
+declare function to_year_month_date_with_slashes(date: Date): string;
+declare function to_full_year_month_date(date: Date): string;
+declare function to_month_date_year_with_slashes(date: Date): string;
+declare function to_date_month_year_with_slashes(date: Date): string;
+declare function to_full_year_month_date_T_hours_Colon_Minutes_Colon_Seconds(date: Date): string;
