3ef34bede0e6e5359110494aa892bbcec52a9d87
yzeek <isaac.cohen1978@gmail.com>
2022-07-12 19:18:09 +0300

    fix commit

--- lib/FormatesEnum.d.ts
+++
@@ -1,9 +0,0 @@
-export declare enum Formats {
-    "YYYY-MM-DD" = "YYYY-MM-DD",
-    "MM/DD/YY" = "MM/DD/YY",
-    "YY/MM/DD" = "YY/MM/DD",
-    "DD/MM/YY" = "YY/MM/YY",
-    "YYYY-MMDDTHH:MM:SS" = "YYYY-MMDDTHH:MM:SS",
-    "YYYYMMDDHHMMSS" = "YYYYMMDDHHMMSS",
-    "YYYY-MM-DD HH:MM" = "YYYY-MM-DD HH:MM"
-}
--- lib/index.d.ts
+++
@@ -1,3 +0,0 @@
-import "./FormatesEnum";
-import "./DateInterface";
-import "./ExtendDate";
